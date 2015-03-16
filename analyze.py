b794891b0fbc9a0198d745b261a3c2d28731ac28
Tom Shen <tom@shen.io>
2015-03-16 01:55:25 -0400

    Implement MRR

--- analyze.py
+++ analyze.py
@@ -5,23 +5,31 @@ import convert
 
 
 def parse_results(results_file):
-    return dict(line.split() for line in results_file)
+    results = {}
+    for line in results_file:
+        toks = line.strip().split('\t')
+        node = toks[0]
+        labels = toks[1:]
+        results[node] = labels
+    return results
 
 
 def parse_test_file(test_file):
     return dict((d, l) for d,l,_ in convert.parse_junto_graph(test_file))
 
 
-def analyze_results(results, doc_labels):
-    for label, nodes in results.items():
-        num_correct = 0
-        num_docs = 0
-        for node in nodes:
-            if node[0] == 'd':
-                num_docs += 1
-                if doc_labels[node] == label:
-                    num_correct += 1
-        print '%s: %.2f%% correct' % (label, float(100 * num_correct)/num_docs)
+def analyze_results(results, node_labels):
+    """ calculates MRR for each label """
+    Q = len(node_labels)
+    inverse_rank_sum = 0.0
+    for node, labels in results.items():
+        if node in node_labels:
+            true_label = node_labels[node]
+            if true_label in labels:
+                inverse_rank_sum += 1.0 / (labels.index(true_label) + 1)
+    mrr = inverse_rank_sum / Q
+    print 'MRR', mrr
+    return mrr
 
 
 def analyze_results_file(results_file, test_file):
@@ -41,13 +49,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.junto_config:
-        test_file = open(convert.parse_junto_config(junto_config)['test_file'])
+        test_file = open(convert.parse_junto_config(args.junto_config)['test_file'])
     elif args.junto_test_file:
         test_file = args.junto_test_file
     else:
         raise parser.error(
             'Must specify either a Junto config file or a Junto test file')
 
-    analyze_results_file(results_file, test_file)
-    results_file.close()
+    analyze_results_file(args.results_file, test_file)
+    args.results_file.close()
     test_file.close()
